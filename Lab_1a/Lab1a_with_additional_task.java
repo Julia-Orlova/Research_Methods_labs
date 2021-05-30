78ec90f9bcb2d7829418bc3e52621d6cee45c876
Julia-Orlova <66700142+Julia-Orlova@users.noreply.github.com>
2021-05-30 22:25:49 +0300

    Create Lab1a_with_additional_task.java

---
+++ Lab_1a/Lab1a_with_additional_task.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView showFactors;
+    private EditText nEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nEdit = (EditText) findViewById(R.id.input);
+        showFactors = (TextView) findViewById(R.id.output);
+
+    }
+
+    public void FermatFactors(View view)
+    {
+        try {
+            long time_begin = System.currentTimeMillis();
+            int n = Integer.parseInt(nEdit.getText().toString());
+            // Перевірка на необхідну додатність числа
+            if (n <= 0) {
+                Toast exc1 = Toast.makeText(this, "Введене число недодатнє, факторизація методом Ферма неможлива", Toast.LENGTH_SHORT);
+                exc1.show();
+                return;
+            }
+
+            int a = (int) Math.ceil(Math.sqrt(n));
+
+            // Перевірка чи n не є квадратним числом
+            if (a * a == n) {
+                double time_end = System.currentTimeMillis();
+                double timeForOperation;
+                timeForOperation = time_end - time_begin;
+                if (timeForOperation > 3) {
+                    System.exit(1);
+                }
+                String res = "Число є квадратом!\n" + a + " * " + a + " = " + n;
+                showFactors.setText(res);
+                return;
+            }
+
+            // Перевірка на парність числа
+            if ((n % 2) == 0) {
+                double time_end = System.currentTimeMillis();
+                double timeForOperation;
+                timeForOperation = time_end - time_begin;
+                if (timeForOperation > 3) {
+                    System.exit(1);
+                }
+                String res = "Число парне!\n" + n/2 + " * " + 2 + " = " + n;
+                showFactors.setText(res);
+                return;
+            }
+
+            int b;
+            while (true) {
+                int b1 = a * a - n;
+                b = (int) (Math.sqrt(b1));
+
+                if (b * b == b1)
+                    break;
+                else
+                    a += 1;
+            }
+
+            double time_end = System.currentTimeMillis();
+            double timeForOperation;
+            timeForOperation = time_end - time_begin;
+            if (timeForOperation > 3) {
+                System.exit(1);
+            }
+            String res = "Результат:\na = " + a + "\nb = " + b + "\n(a - b)(a + b) = " + (a - b) + " * " + (a + b) + " = " + n;
+            showFactors.setText(res);
+
+
+        } catch (Exception e) {
+            Toast exc2 = Toast.makeText(this, "Некоректний ввід", Toast.LENGTH_SHORT);
+            exc2.show();
+        }
+    }
+}
